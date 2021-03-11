1e960a9fc6c2bbb7a00fea2fc3eb54bac40b4888
Titania01 <adegbitebidmus@gmail.com>
2021-03-11 17:50:28 +0100

    feat: data from server

--- src/index.js
+++ src/index.js
@@ -1,48 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx'
-import axios from 'axios'
-
-const promise = axios.get('http://localhost:3001/notes')
-
-promise.then(response => {
-  console.log(response)
-})
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
 
 
+// const promise = axios.get('http://localhost:3001/notes')
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+// promise.then(response => {
+//   console.log(response)
+// })
+
+
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App notes={notes}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2019-05-30T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     date: '2019-05-30T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2019-05-30T19:20:14.298Z',
+//     important: true
+//   }
+// ]
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
