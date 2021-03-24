3093382f0b9c9a693b46a824020769d670be1730
Titania01 <adegbitebidmus@gmail.com>
2021-03-24 15:44:21 +0100

    feat: promises and errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import Note from './components/Note'
+import noteService from "./services/notes"
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
   const toggleImportanceOf = (id) => {
     const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
 
     axios.put(url, changedNote).then(response => {
       setNotes(notes.map(note => note.id !== id ? note: response.data))
@@ -39,6 +54,13 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
     }
+
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
 
     axios
       .post('http://localhost:3001/notes', noteObject)
